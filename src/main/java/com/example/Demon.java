7ce44fcc8ab659707cbb93b94819736db4b07857
banana <admin@bananajump.com>
2024-08-24 22:09:09 -0500

    use maven

--- src/main/java/com/example/Demon.java
+++ src/main/java/com/example/Demon.java
@@ -1,3 +1,5 @@
+package com.example;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
--- src/main/java/com/example/Entity.java
+++ src/main/java/com/example/Entity.java
@@ -1,3 +1,5 @@
+package com.example;
+
 public abstract class Entity {
     public abstract void move(Entity[][] board, int currentTurn);
     public abstract void mult(Entity[][] board);
